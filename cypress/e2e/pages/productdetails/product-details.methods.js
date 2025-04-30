0d1f911573de3270c21eb5c6ce649ff58f594d8d
Nahir <nahircufre@gmail.com>
2025-04-30 14:42:55 -0300

    creacion de archivos a product details

---
+++ cypress/e2e/pages/productdetails/product-details.methods.js
@@ -0,0 +1,7 @@
+import { productDetailsElements } from "./product-details.element";
+
+export class productDetailsMethods{
+    static clickOnAddToCartButton(){
+        productDetailsElements.buttons.addToCart.click();
+    }
+}
