54589e065f7bbcdd35d31fd2a786a3fb5ea500da
Nahir <nahircufre@gmail.com>
2025-04-30 17:23:23 -0300

    creacion pagina card

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,16 @@
 import { loginMethods } from "./pages/login/login.methods"
 import { homeMethods } from "./pages/homepage/home.methods"
+import { cartMethods } from "./pages/cart/cart.methods"
+
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.demoblaze.com/')
+    cy.visit('https://www.demoblaze.com/cart.html')
     //cy.get('a[data-target="#logInModal"]').click()
    // loginMethods.login('username','password')
-   homeMethods.clickOnProductLink('Iphone 6 32gb')
+   //homeMethods.clickOnProductLink('Iphone 6 32gb')
+    cy.wait(20000)
+    cartMethods.clickOnDeleteLink('Iphone 6 32gb')
     cy.wait(5000)
   })
 })
