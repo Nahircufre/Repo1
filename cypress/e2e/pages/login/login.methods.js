c30d0228f585e37eeadf127071cf77155c556522
Nahir <nahircufre@gmail.com>
2025-06-18 11:28:32 -0300

    log-in invalido

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -1,25 +1,29 @@
-import { logger } from "../../util/logger"
-import { loginElements } from "./login.elements"
+import { logger } from "../../util/logger";
+import { commonPageMethods } from "../common-page/common-page.methods";
+import { loginElements } from "./login.elements";
 
-export class loginMethods{
-   static insertUsername(username){
-        loginElements.textBoxes.username.type(username)
-   } 
-   
-    static insertPassword(password){
-        loginElements.textBoxes.password.type(password)
-    }
-    static clickOnLoginButton(){
-        loginElements.buttons.login.click()
-    }
+export class loginMethods {
+  static insertUsername(username) {
+    loginElements.textBoxes.username.type(username);
+  }
 
-    static login(username, password){
-        logger.subStep('insertar username')
-        this.insertUsername(username)
-        logger.subStep('insert password')
-        this.insertPassword(password)
-        logger.subStep('clickonloguin')
-        this.clickOnLoginButton()
-    }
+  static insertPassword(password) {
+    loginElements.textBoxes.password.type(password);
+  }
+  static clickOnLoginButton() {
+    loginElements.buttons.login.click();
+  }
 
+  static login(username, password) {
+    logger.subStep("insertar username");
+    this.insertUsername(username);
+    logger.subStep("insert password");
+    this.insertPassword(password);
+    logger.subStep("clickonloguin");
+    this.clickOnLoginButton();
+  }
+
+  static verifyWrongPassword() {
+    commonPageMethods.verifyAlert("Wrong Password");
+  }
 }
---
+++ cypress/e2e/test/auntenticacion-invalida.cy.js
@@ -0,0 +1,30 @@
+import { commonPageData } from "../pages/common-page/common-page.data";
+import { commonPageMethods } from "../pages/common-page/common-page.methods";
+import { LoginData } from "../pages/login/login.data";
+import { loginMethods } from "../pages/login/login.methods";
+import { logger } from "../util/logger";
+
+describe(commonPageData.testSuites.autenticacion, () => {
+  it("Inicio de sesion invalido", () => {
+    logger.stepNumber(1);
+    logger.step("Navgera a la pagina de inicio");
+    commonPageMethods.navigateToDemoBlaze();
+
+    logger.stepNumber(2);
+    logger.step("hacer click en log in");
+    commonPageMethods.clickOnLogIn();
+
+    logger.stepNumber(3);
+    logger.step("Ingresar nombre de usuario y contra invalidos");
+    loginMethods.insertUsername(LoginData.validCredentials.username);
+    loginMethods.insertPassword("contrasena");
+
+    logger.stepNumber(4);
+    logger.step("Hacer click en login button");
+    loginMethods.clickOnLoginButton();
+    logger.verification(
+      "verificar q el usuario se dirige a la pagina de inicio"
+    );
+    loginMethods.verifyWrongPassword();
+  });
+});
